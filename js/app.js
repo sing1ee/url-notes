e69376b9d403f0911ca60a4dea3bd7dc74d1c8db
zhangcheng <zh.milo@gmail.com>
2025-01-29 17:17:42 +0800

    favicon

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const archives = [
-        '2024-12.md',
-        '2025-01.md'
+        '2025-01.md',
+        '2024-12.md'
     ]
     
     renderArchiveList(archives)
